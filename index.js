0da9376b9f339d3d388ff371408d55646c13fff8
Eric Kelvin Waweru <kelsric@yahoo.com>
2022-06-12 19:56:36 +0300

    commit tree f1ffe246822c02d64e94dc497e443c226c7f72e3

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 // Write your solution in this file!
-var customerName = "bob"
-var bestCustomer = undefined
-const leastFavoriteCustomer = "You can't change me"
-function upperCaseCustomerName(){
-    customerName = customerName.toUpperCase();
-}
-
-
-function setBestCustomer(){
-  bestCustomer = "not bob"
-}
-
-function overwriteBestCustomer(){
-  bestCustomer = "maybe bob"
-}
-
-function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer = "Wait a minute I shoudn't work. "
-}
+const tutorials = [
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
+  
+  const titleCased = () => {
+    return tutorials
+  }
